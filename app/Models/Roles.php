0d430c5ff5b995f7a6ec3f6ca65204fdb551569d
angel <angel.rodriguezafa@gmai.com>
2025-03-29 23:54:46 -0500

    repositorio

---
+++ app/Models/Roles.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Roles extends Model
+{
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = ['nombre'];
+   
+
+    // Relación: Un rol puede tener muchos usuarios
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
+}
+
